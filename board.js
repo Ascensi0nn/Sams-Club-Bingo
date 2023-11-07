703825dc4cddb6b32441ea1e8fd177cbd7ba5d29
Owen Luts <oluts47@gmail.com>
2023-11-07 00:14:19 -0500

    Version 1 complete

--- board.js
+++ board.js
@@ -1,5 +1,7 @@
 const numOfNodes = 25;
 const nodeHolder = document.getElementById('node-holder');
+let expireMs = localStorage.getItem('expMs');
+let board = JSON.parse(localStorage.getItem("boardOrder"));
 
 const sayings = [
     "Gio says 'Right'",
@@ -10,6 +12,8 @@ const sayings = [
     "Gio wears a sweater",
     "Gio complains about his discrete professor",
     "Gio tells you to burn in hell",
+    "Gio starts singing",
+    "Gio references a musical (drama shit)",
 
     "Jaysen acts gay",
     "Jaysen says 'I swear!'",
@@ -18,6 +22,7 @@ const sayings = [
     "Jaysen recognizes the most obscure reference",
     "Jaysen spits the most random fact ever",
     "Jaysen shows up late to pickleball",
+    "Jaysen says a random sports fact",
 
     "Jack mentions a programming term",
     "Jack finishes a meal (impossible)",
@@ -32,6 +37,8 @@ const sayings = [
     "Amy dyes her hair a new color",
     "Amy sticks her tongue out (scaring Jack and Owen)",
     "Amy sleeps through Chem",
+    "Amy reads porn (in public)",
+    "Amy turns into a human cannon and attacks owen with applesauce",
 
     "Max goes to commons for dinner",
     "Max goes AWOL for a whole day",
@@ -47,8 +54,10 @@ const sayings = [
     "Mary's phone is at 1%",
     "Mary Ann takes obscene amount of photos",
     "Mary is cold",
+    "Mary Ann admits to a felony",
 
     "Clark studies in Owen's room",
+    "Clark's phone gets shattered",
     
     "Sam disappears for weeks at a time",
     "Sam asks us to meet on the Greek lawn at 5am",
@@ -61,12 +70,14 @@ const sayings = [
     
     "Dante eats something vegan",
     "Dante stiffly nods",
+    "Dante complains about Sol",
     
     "Kiera plays that rhythm game",
     "Kiera wears green",
     "Kiera sarcastically laughs loudly in public",
     "Kiera does the 'um actually!' voice",
     "Kiera gets Starbucks",
+    "Kiera says 'excuse me'",
 
     "Mikey gets high as a whistle",
     "Mikey doesn't remember the weekend",
@@ -86,9 +97,7 @@ function createBoard() {
         const nodeTxt = document.createElement('p');
 
         node.classList.add('node');
-        node.classList.add('unchecked');
         nodeTxt.classList.add('txt');
-        nodeTxt.classList.add('txtUnchecked');
         node.id = 'node-' + i;
         nodeTxt.id = 'node-' + i + '-txt';
         node.addEventListener('click', () => {
@@ -97,23 +106,73 @@ function createBoard() {
                 node.classList.remove('checked');
                 nodeTxt.classList.add('txtUnchecked');
                 nodeTxt.classList.remove('txtChecked');
+                board[i]['checked'] = 'unchecked';
+                board[i]['txtChecked'] = 'txtUnchecked';
             }
             else if (node.classList.contains('unchecked')) {
                 node.classList.add('checked');
                 node.classList.remove('unchecked');
                 nodeTxt.classList.add('txtChecked');
                 nodeTxt.classList.remove('txtUnchecked');
+                board[i]['checked'] = 'checked';
+                board[i]['txtChecked'] = 'txtChecked';
             }
+            localStorage.setItem('boardOrder', JSON.stringify(board));
         });
 
         node.appendChild(nodeTxt);
         nodeHolder.appendChild(node);
     }
-    randomizeNodes();
+    generateNodes();
+}
+
+function checkDate() {
+    let d = new Date();
+    d.setDate(d.getDate() + 1);
+    d.setHours(0);
+    d.setMinutes(0);
+    d.setSeconds(0);
+    d.setMilliseconds(0);
+
+    // guard clause :)
+    if (expireMs == null) {
+        expireMs = d.getTime();
+        localStorage.setItem('expMs', expireMs);
+        return true;
+    }
+
+    const expirationTime = parseInt(expireMs);
+    if (expirationTime <= Date.now()) {
+        expireMs = d.getTime();
+        localStorage.setItem('expMs', expireMs);
+        return true;
+    }
+
+    return false;
+}
+
+function generateNodes() {
+    if (checkDate()) {
+        randomizeNodes();
+    }
+    else {
+        getNodesFromLocal();
+    }
+}
+
+function getNodesFromLocal() {
+    for (let i = 0; i < board.length; i++) {
+        const node = document.getElementById('node-' + i);
+        const nodeTxt = document.getElementById('node-' + i + '-txt');
+        nodeTxt.innerText = sayings[board[i]['index']];
+        node.classList.add(board[i]['checked']);
+        nodeTxt.classList.add(board[i]['txtChecked']);
+    }
 }
 
 function randomizeNodes() {
     let currentSayings = [];
+    let nodes = [];
     for (let i = 0; i < numOfNodes; i++) {
         const node = document.getElementById('node-' + i);
         node.classList.remove('checked');
@@ -130,24 +189,34 @@ function randomizeNodes() {
         
         nodeTxt.innerText = sayings[random];
         currentSayings.push(random);
+
+        nodes.push({
+            index: random,
+            checked: 'unchecked',
+            txtChecked: 'txtUnchecked'
+        });    
+    }
+
+    board = nodes;
+    localStorage.setItem('boardOrder', JSON.stringify(nodes));
+}
+
+const runTimer = () => {
+    const current = new Date();
+
+    const hours = (23 - current.getHours()).toString().padStart(2, '0');
+    const minutes = (59 - current.getMinutes()).toString().padStart(2, '0');
+    const seconds = (59 - current.getSeconds()).toString().padStart(2, '0');
+
+    const timeDiv = document.getElementById('time');
+    timeDiv.innerText = `${hours}:${minutes}:${seconds}`;
+
+    if (expireMs <= Date.now()) {
+        setTimeout(() => {
+            window.location.reload();
+        }, 1000);
     }
 }
 
-document.getElementById('randomizer').addEventListener('click', () => {
-    randomizeNodes();
-});
-
-
-let timeout = null;
-const shuffle = document.getElementById('randomizer');
-shuffle.addEventListener('mouseover', function(e) {
-    if(timeout !== null) return;
-    e.target.classList.add('box-rotate');
-    timeout = setTimeout(function() {
-        e.target.classList.remove('box-rotate');
-        timeout = null;
-    }, 1000);
-});
-
-
 createBoard();
+setInterval(runTimer, 1000);
--- index.js
+++
@@ -1,3 +0,0 @@
-document.getElementById('button').addEventListener('click', () => {
-    window.location.href = "./board";
-});
